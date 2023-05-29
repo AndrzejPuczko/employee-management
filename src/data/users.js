75035701f63e2e99905f11049615d5cfc589a5c0
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-29 15:39:06 +0200

    feat: add the first component with a list of employees

---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+export const users = [
+	{
+		name: 'Andrzej Puczko',
+		lengthOfService: '1 year 128 days',
+		average: '2.3',
+	},
+	{
+		name: 'Adam Rucinski',
+		lengthOfService: '2 year 128 days',
+		average: '4.3',
+	},
+	{
+		name: 'Mariusz Blaszczak',
+		lengthOfService: '0 year 128 days',
+		average: '1.3',
+	},
+	{
+		name: 'Robert Kozera',
+		lengthOfService: '3 year 128 days',
+		average: '3.3',
+	},
+]
