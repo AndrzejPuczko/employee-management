864243adff7cd6bbca0c34e8a0114365f026282b
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-30 21:22:42 +0200

    feat: add first styled component to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import UsersList from './components/UsersList/UsersList'
+import './assets/GlobalStyles.scss'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	background-color: #f7f8fa;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100vh;
+`
 
 function App() {
 	return (
-		<>
+		<Wrapper>
 			<UsersList />
-		</>
+		</Wrapper>
 	)
 }
 
