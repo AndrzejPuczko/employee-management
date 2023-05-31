4ddb2144bfd45dcb49a16f994665babd60f273ac
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-31 20:01:11 +0200

    feat: add button component

---
+++ src/components/_elements/Button/Button.jsx
@@ -0,0 +1,10 @@
+import { StyledButton } from './Button.styles'
+import { ReactComponent as DeleteIcon } from '/@/assets/icons/delete-icon.svg'
+
+const Button = () => (
+	<StyledButton>
+		<DeleteIcon />
+	</StyledButton>
+)
+
+export default Button
