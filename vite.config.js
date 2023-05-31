790f9ab13eb93b260cb4ec010f66c073d05a9614
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-31 16:14:10 +0200

    feat: add add svgr and path plugin and remove css module rule

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from 'path'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), svgr()],
 	base: '',
 	build: {
 		outDir: './dist',
 	},
-	css: {
-		modules: {
-			localsConvention: 'camelCase',
-			generateScopedName: '[name]_[local]__[hash:base64:5]',
+	resolve: {
+		alias: {
+			'/@': path.resolve(__dirname, 'src'),
 		},
 	},
 })
