6663e900dcee3c90e30126b0d626bfb157abc9a7
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-05-31 19:59:58 +0200

    fix: add two person to users

--- src/data/users.js
+++ src/data/users.js
@@ -19,4 +19,14 @@ export const users = [
 		lengthOfService: '3 year 128 days',
 		average: '3.3',
 	},
+	{
+		name: 'Adam Rucinski',
+		lengthOfService: '2 year 128 days',
+		average: '4.3',
+	},
+	{
+		name: 'Mariusz Blaszczak',
+		lengthOfService: '0 year 128 days',
+		average: '1.3',
+	},
 ]
