e9978aa3d3f459c964181da81b2b277ff050ce40
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-02 21:15:26 +0200

    fix: add id for users

--- src/data/users.js
+++ src/data/users.js
@@ -3,30 +3,36 @@ export const users = [
 		name: 'Andrzej Puczko',
 		lengthOfService: '1 year 128 days',
 		average: '2.3',
+		id: 0,
 	},
 	{
 		name: 'Adam Rucinski',
 		lengthOfService: '2 year 128 days',
 		average: '4.3',
+		id: 1,
 	},
 	{
 		name: 'Mariusz Blaszczak',
 		lengthOfService: '0 year 128 days',
 		average: '1.3',
+		id: 2,
 	},
 	{
 		name: 'Robert Kozera',
 		lengthOfService: '3 year 128 days',
 		average: '3.3',
+		id: 3,
 	},
 	{
 		name: 'Adam Rucinski',
 		lengthOfService: '2 year 128 days',
 		average: '4.3',
+		id: 4,
 	},
 	{
 		name: 'Mariusz Blaszczak',
 		lengthOfService: '0 year 128 days',
 		average: '1.3',
+		id: 5,
 	},
 ]
