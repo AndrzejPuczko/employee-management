ec5f2f5224bf6a7589996e66e8214b6637e029ba
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-05 21:13:56 +0200

    feat: add SearchBar and NewsSection component

--- src/components/layouts/MainLayout/MainLayout.jsx
+++ src/components/layouts/MainLayout/MainLayout.jsx
@@ -1,11 +1,20 @@
+
 import Navigation from '../../_templates/Navigation/Navigation'
 import { Wrapper } from './MainLayout.styles'
+import SearchBar from '../../_templates/SearchBar/SearchBar'
+import NewsSection from '../../_templates/NewsSection/NewsSection'
+
+
+
+
 
 const MainLayout = ({ children }) => {
 	return (
 		<Wrapper>
 			<Navigation />
+			<SearchBar />
 			{children}
+			<NewsSection />
 		</Wrapper>
 	)
 }
