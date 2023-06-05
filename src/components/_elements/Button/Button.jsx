08a0780934e43ecd912afb2ebbc661c5e01fcd87
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-05 21:18:20 +0200

    fix: add styled rules in Button

--- src/components/_elements/Button/Button.jsx
+++ src/components/_elements/Button/Button.jsx
@@ -2,14 +2,14 @@ import styled from 'styled-components'
 
 const Button = styled.button`
 	margin: 15px 0;
-	padding: 7px 20px;
-	font-size: ${({ theme }) => theme.fontSize.s};
+	padding: ${({ isBig }) => (isBig ? '10px 70px' : '7px 20px')};
+	font-size: ${({ isBig, theme}) => (isBig ? theme.fontSize.lg : theme.fontSize.s)};
 	background-color: ${({ theme }) => theme.colors.lightPurple};
 	border-radius: 20px;
 	border: none;
 	font-weight: bold;
 	color: ${({ theme }) => theme.colors.darkGrey};
-    cursor: pointer;
+	cursor: pointer;
 `
 
 export default Button
