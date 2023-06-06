7c64b2edacdd012e2c82db2b665aacdfa37c8eec
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-06 20:25:58 +0200

    fix: use => useForm hook in project

--- src/Views/AddUser.jsx
+++ src/Views/AddUser.jsx
@@ -4,6 +4,7 @@ import Button from '../components/_elements/Button/Button'
 import { ViewWrapper } from '../components/_modules/ViewWrapper/ViewWrapper'
 import { Title } from '../components/_elements/Title/Title'
 import { UsersContext } from '../providers/UsersProvider'
+import { useForm } from '../hooks/useForm'
 
 const initialFormState = {
 	name: '',
@@ -13,50 +14,18 @@ const initialFormState = {
 	error: '',
 }
 
-const reducer = (state, action) => {
-	switch (action.type) {
-		case 'INPUT CHANGE':
-			return {
-				...state,
-				[action.field]: action.value,
-			}
-		case 'CLEAR VALUE':
-			return initialFormState
-		case 'RULES':
-			return {
-				...state,
-				rules: !state.rules,
-			}
-		case 'THROW ERROR':
-			return {
-				...state,
-				error: action.errorValue,
-			}
-		default:
-			break
-	}
-}
-
 const AddUser = () => {
-	const [formValues, dispatch] = useReducer(reducer, initialFormState)
 	const { handleAddUser } = useContext(UsersContext)
-
-	const handleInputChange = e => {
-		dispatch({
-			type: 'INPUT CHANGE',
-			field: e.target.name,
-			value: e.target.value,
-		})
-	}
+	const { formValues, handleInputChange, handleClearForm, handleThrowError, handleToggleRules } = useForm(initialFormState)
 
 	const handleSubmitUser = e => {
 		e.preventDefault()
 
 		if (formValues.rules) {
 			handleAddUser(formValues)
-			dispatch({ type: 'CLEAR VALUE' })
+			handleClearForm(initialFormState)
 		} else {
-			dispatch({ type: 'THROW ERROR', errorValue: 'You need to accept the rules' })
+			handleThrowError('You need to accept the rules')
 		}
 	}
 
@@ -66,7 +35,7 @@ const AddUser = () => {
 			<FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
 			<FormField label="Length Of Service" id="lengthOfService" name="lengthOfService" value={formValues.lengthOfService} onChange={handleInputChange} />
 			<FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange} />
-			<FormField label="Rules" id="rules" name="rules" type="checkbox" value={formValues.rules} onChange={() => dispatch({ type: 'RULES' })} />
+			<FormField label="Rules" id="rules" name="rules" type="checkbox" value={formValues.rules} onChange={handleToggleRules} />
 			<Button type="submit">Add</Button>
 			{formValues.error && <p>{formValues.error}</p>}
 		</ViewWrapper>
