23ccf046cf08e1c09ed2a4daf667ac7b83be64be
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-11 19:09:39 +0200

    fix: remove Links

--- src/components/_templates/Navigation/Navigation.jsx
+++ src/components/_templates/Navigation/Navigation.jsx
@@ -11,10 +11,9 @@ const Navigation = () => {
 					Management
 				</h1>
 			</Logo>
-			<StyledLink to="/">Dashboard</StyledLink>
+			<StyledLink to="/departments">Dashboard</StyledLink>
 			<StyledLink to="/add-user">Add user</StyledLink>
-			<StyledLink to="/">Settings</StyledLink>
-			<StyledLink to="/">Logout</StyledLink>
+	
 		</Wrapper>
 	)
 }
