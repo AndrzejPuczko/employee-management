b31043e81f6a25e79906b40572eb0b2f9eba0e38
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-06-11 19:03:54 +0200

    feat: add connect to mock API

--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -1,14 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ViewWrapper } from '../components/_modules/ViewWrapper/ViewWrapper'
 import UsersList from '../components/_templates/UsersList/UsersList'
-import { UsersContext } from '../providers/UsersProvider'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Dashboard = () => {
-	const { user } = useContext(UsersContext)
+	const { id } = useParams()
+	const [employees, setEmployees] = useState([])
+	const [departments, setDepartments] = useState([])
+
+	useEffect(() => {
+		axios
+			.get(`/departments`)
+			.then(({ data }) => setDepartments(data))
+			.catch(err => console.log(err))
+	}, [])
+
+	useEffect(() => {
+		axios
+			.get(`/employees/${id || departments[0]}`)
+			.then(({ data }) => setEmployees(data.employees))
+			.catch(err => console.log(err))
+	}, [id, departments])
 
 	return (
 		<ViewWrapper>
-			<UsersList user={user} />
+			<nav>
+				{departments.map(department => (
+					<Link key={department} to={`/departments/${department}`}>
+						{department}{' '}
+					</Link>
+				))}
+			</nav>
+			<UsersList user={employees} />
 		</ViewWrapper>
 	)
 }
